1dbfaa1a42be60b5a58303ab9161884a6018f353
gulcearda <77541794+gulcearda@users.noreply.github.com>
2021-01-18 11:12:24 +0300

    2. Deneme

--- Deneme/Program.cs
+++ Deneme/Program.cs
@@ -7,8 +7,18 @@ namespace Deneme
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
             Console.WriteLine("Hello World!");
+
+
             Console.WriteLine("Hello World!");
+
+
+            Console.WriteLine("Hello World!");
+
+
+            Console.WriteLine("Hello World!");
+
         }
     }
 }
